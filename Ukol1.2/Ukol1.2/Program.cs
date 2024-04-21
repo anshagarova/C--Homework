0304751a67bf1f446eca9fd3931a69122a1b20c5
anshagarova <149403028+anshagarova@users.noreply.github.com>
2024-04-21 12:56:01 +0200

    Ukol 1.2

---
+++ Ukol1.2/Ukol1.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        do
+        {
+            Console.WriteLine("Zadej počet hvězdiček:");
+            string vstup = Console.ReadLine();
+            int pocetHvezdicek;
+
+            if (!int.TryParse(vstup, out pocetHvezdicek))
+            {
+                Console.WriteLine("Neplatný vstup. Zadejte prosím celé číslo.");
+                continue; 
+            }
+
+            for (int i = 0; i < pocetHvezdicek; i++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine(); 
+
+            Console.WriteLine("Chcete spustit program znovu? (ano/ne)");
+        } while (Console.ReadLine().ToLower() == "ano");
+    }
+}
