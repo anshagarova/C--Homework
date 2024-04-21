b5991988b87d49f15f56ae31fc5421584e196422
anshagarova <149403028+anshagarova@users.noreply.github.com>
2024-04-21 13:24:59 +0200

    Ukol 1.3

---
+++ Ukol1.3/Ukol1.3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Lucistnik
+{
+    public int PocetSipu { get; private set; }
+
+    public Lucistnik(int pocetSipu)
+    {
+        PocetSipu = pocetSipu;
+    }
+
+    public void Vystrel()
+    {
+        while (PocetSipu > 0)
+        {
+            Console.WriteLine("Vždy se trefím přímo do prostředí!");
+            PocetSipu--;
+        }
+        if (PocetSipu <= 0)
+        {
+            Console.WriteLine("Nemám šípy.");
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Lucistnik lucistnik = new Lucistnik(5);
+        while (lucistnik.PocetSipu > 0)
+        {
+            lucistnik.Vystrel();
+        }
+    }
+}
